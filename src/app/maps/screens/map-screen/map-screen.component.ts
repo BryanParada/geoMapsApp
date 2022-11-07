11c1e33b034f204a901dd11919fa60265a2b9f12
Bryan Parada <bryan.parada.cea@gmail.com>
2022-11-07 15:07:51 -0300

    V414 - Folder structure

---
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-map-screen',
+  templateUrl: './map-screen.component.html' 
+})
+export class MapScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
